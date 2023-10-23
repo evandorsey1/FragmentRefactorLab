b618b85a0a37a80728ca114c8428876e87d0f2ca
Evan Dorsey <dorseyevan@gmail.com>
2023-10-23 16:03:12 -0400

    Added to the skeleton, still have to add FragmentTransaction

--- app/src/main/java/edu/temple/fragmentrefactor/ActivityFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/ActivityFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class ActivityFragment : Fragment() {
@@ -14,11 +17,28 @@ class ActivityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        return inflater.inflate(R.layout.fragment_activity, container, false).apply{
+
+                val nameEditText = findViewById<EditText>(R.id.nameEditText)
+                val changeButton = findViewById<Button>(R.id.changeButton)
+                val displayTextView = findViewById<TextView>(R.id.displayTextView)
 
 
+                changeButton.setOnClickListener {
+
+                    var name = nameEditText.text.toString()
+
+                    displayTextView.text = if (name.isNotEmpty()) {
+                        "Hello, $name!"
+                    } else {
+                        "Please enter your name"
+                    }
+                }
 
 
+        }
     }
 }
